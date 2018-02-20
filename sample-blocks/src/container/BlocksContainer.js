6b56c26e8b19e27e4c56d920802823540b04707b
ethanryan <ethanryan@gmail.com>
2018-02-20 17:40:27 -0500

    added container folder and BlocksContainer.js

---
+++ sample-blocks/src/container/BlocksContainer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class BlocksContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        this is the BlocksContainer.
+      </div>
+    )
+  }
+
+}
+export default BlocksContainer
