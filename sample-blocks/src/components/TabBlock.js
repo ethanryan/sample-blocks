68453ca257d09c9b70b03ee7e7feafdccd715e82
ethanryan <ethanryan@gmail.com>
2018-02-20 17:55:14 -0500

    added Semantic UI React, updated TabBlock

--- sample-blocks/src/components/TabBlock.js
+++ sample-blocks/src/components/TabBlock.js
@@ -1,19 +1,32 @@
-import React, {Component} from 'react'
+import React from 'react'
+// import React, {Component} from 'react'
 
-class TabBlock extends Component {
-  constructor() {
-    super()
-    this.state = {
-    }
-  }
+import { Tab } from 'semantic-ui-react'
 
-  render() {
-    return(
-      <h1>
-        this is the TabBlock.
-      </h1>
-    )
-  }
+const panes = [
+  {menuItem: 'Tab1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+  {menuItem: 'Tab2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+  {menuItem: 'Tab3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+]
 
-}
+const TabBlock = () => (
+  <Tab panes={panes} />
+)
+
+// class TabBlock extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//     }
+//   }
+//
+//   render() {
+//     return(
+//       <h1>
+//         this is the TabBlock.
+//       </h1>
+//     )
+//   }
+//
+// }
 export default TabBlock
