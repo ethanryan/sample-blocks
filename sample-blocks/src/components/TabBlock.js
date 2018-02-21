a1ab341cf08fd881e5fb4623d6dcfb75b79eff36
ethanryan <ethanryan@gmail.com>
2018-02-21 01:18:06 -0500

    parsing jsonArray, rendering images in tabs

--- sample-blocks/src/components/TabBlock.js
+++ sample-blocks/src/components/TabBlock.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-import { Tab, Menu, Image } from 'semantic-ui-react'
+import { Tab } from 'semantic-ui-react'
 
-const panes = [
+var Parser = require('html-react-parser')
+
+const jsonArray = [
+// const panes = [
 //   { menuItem: 'Embracing Discovery', render: () =>
 //   <Tab.Pane>Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.
 //     <Image
@@ -12,7 +15,7 @@ const panes = [
 //     />
 // </Tab.Pane>
 // },
-  { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", image: "https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif", },
+  { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><Image src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' />", },
   // { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work." },
   { menuItem: "Tab 2", pane: "Tab 2 Content is right here 222", },
   { menuItem: "Tab 3", pane: "Tab 3 Content is right here 333", },
@@ -20,41 +23,30 @@ const panes = [
   // { menuItem: "Tab 4", pane: "Tab 4 Content is right here mofooooo", image: "https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif" },
 ]
 
-// function parsePanes(jsonArray) {
-//   return jsonArray.map(object =>
-//     <div>
-//       <Menu.Item>{object.menuItem}</Menu.Item>
-//       <Tab.Pane>{object.pane}</Tab.Pane>
-//       <Image src={object.image} />
-//     </div>
-//   )
-// }
 
-// var x = parsePanes(panes)
+const parsedPanes = jsonArray.map( (object, index) => {
+  console.log('TabBlock - object is: ', index, object)
+  var newObject = Object.assign({}, object)
+  // newObject.key = index
+  newObject.pane = Parser(object.pane) //second key value is parsed
+  ////////trying to add unique key to each child in array so i don't get warning in console...
+  // if (newObject.pane instanceof Array) {
+  //   console.log('adding keys to array of parsed object...')
+  //   newObject.pane = newObject.pane.map( (object, index) => {
+  //     console.log(`------returning ${object} with div key ${index}`)
+  //     return <div key={index}>{object}</div>
+  //   })
+  // }
+  // console.log('TabBlock - newObject is: ', newObject)
+  return newObject
+})
+
+console.log('TabBlock - jsonArray is::::: ', jsonArray)
+console.log('TabBlock - parsedPanes is::::: ', parsedPanes)
+
 
 const TabBlock = () => (
-  <Tab panes={panes} renderActiveOnly={false} />
-  // <Tab panes={panes} />
-  /* <Tab panes={parsePanes(panes)} /> */
-  // <Tab panes={x} />
+  <Tab panes={parsedPanes} renderActiveOnly={false} />
 )
-
-// const TabBlock = () => {
-//   return (
-//     <div>
-//       <Tab panes={x} />
-//     </div>
-//   )
-// }
-
-// const TabBlock = (props) => {
-//   return (
-//     <div>
-//       {parsePanes(panes)}
-//     </div>
-//   )
-// }
-
-
 
 export default TabBlock
