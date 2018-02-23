8c5b50b6e5be5d9968006f3decc6b42352f4fe73
ethanryan <ethanryan@gmail.com>
2018-02-22 22:42:58 -0500

    cleaning up code

--- sample-blocks/src/components/TabBlock.js
+++ sample-blocks/src/components/TabBlock.js
@@ -23,9 +23,7 @@ const jsonArray = [
   { menuItem: "Making It Real", pane: "No creative process is truly complete until it manifests a tangible reality. Whether your idea is an action or a physical creation, bringing it to life will likely involve the hard work of iteration, testing, and refinement. <br><br>Just be wary of perfectionism. Push yourself to share your creations with others. By maintaining an open stance, you’ll be able to learn from their feedback. Consider their responses new material that you can draw from the next time you’re embarking on a creative endeavor.", },
   { menuItem: "Love The Work", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><br><Image className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' fluid />", },
   { menuItem: "Have Fun", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", },
-  { menuItem: "Tab 666", pane: "Tab 666 Content is right here 666 ---- Tab 666 Content is right here 666 ---- Tab 666 Content is right here 666 ----- Tab 666 Content is right here 666 ----- Tab 666 Content is right here 666 ---- Tab 666 Content is right here 666", },
-  { menuItem: "Tab 777", pane: "Tab 777 Content is right here 777 ---- Tab 777 Content is right here 777 ---- Tab 777 Content is right here 777 ----- Tab 777 Content is right here 777 ----- Tab 777 Content is right here 777 ---- Tab 777 Content is right here 777", },
-  { menuItem: " .. ", pane: " last tab here..... ", },
+  { menuItem: " ", pane: " ", },
   // { menuItem: "Tab 4", pane: "Tab 4 Content is right here mofooooo", image: "https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif" },
 ]
 
