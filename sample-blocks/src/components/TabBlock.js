6888c6c10acff625d6641f85808386d870e0118f
ethanryan <ethanryan@gmail.com>
2018-02-26 14:18:23 -0500

    added typeface-merriweather via npm install

--- sample-blocks/src/components/TabBlock.js
+++ sample-blocks/src/components/TabBlock.js
@@ -9,27 +9,19 @@ const TabBlock = (props) => {
   console.log('99999999999 -------- TabBlock props: ', props)
 
   var Parser = require('html-react-parser')
+  require('typeface-merriweather')
+
 
   const jsonArray = [
-    { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><br><Image id='imgOne' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' fluid /><div id='zoomedDivOne' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'> <div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomOne' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(40.3438px, -329px, 0px) scale(1.09492); transform-origin: center center 0px; will-change: transform, top, left; top: 361px; left: 252.156px; width: 619px; height: 380px;'></div>", },
+    { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.<br><br><Image id='imgOne' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' fluid /><div id='zoomedDivOne' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'> <div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomOne' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(40.3438px, -329px, 0px) scale(1.09492); transform-origin: center center 0px; will-change: transform, top, left; top: 361px; left: 252.156px; width: 619px; height: 380px;'></div>", },
     { menuItem: "Gaining Insight", pane: "To spark creativity, feed your brain material like you're cramming for a tough test. Then stop thinking about the problem you want to solve. Go surfing or take a leisurely walk. Research shows that letting your mind wander fosters creativity. <br></br>It’s also found that meditation helps you spot and solve problems in creative ways. It promotes divergent thinking that gets novel ideas flowing. According to these studies, meditation also makes you more open to considering new solutions. Time to breathe.", },
-    { menuItem: "Making It Real", pane: "No creative process is truly complete until it manifests a tangible reality. Whether your idea is an action or a physical creation, bringing it to life will likely involve the hard work of iteration, testing, and refinement. <br><br>Just be wary of perfectionism. Push yourself to share your creations with others. By maintaining an open stance, you’ll be able to learn from their feedback. Consider their responses new material that you can draw from the next time you’re embarking on a creative endeavor.", },
-    { menuItem: "Love The Work", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><br><Image id='imgTwo' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' fluid /><div id='zoomedDivTwo' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'><div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomTwo' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(159.844px, -236px, 0px) scale(1.04653); transform-origin: center center 0px; will-change: transform, top, left; top: 263px; left: 248.156px; width: 380px; height: 380px;'></div>", },
+    { menuItem: "Making It Real", pane: "No creative process is truly complete until it manifests a tangible reality. Whether your idea is an action or a physical creation, bringing it to life will likely involve the hard work of iteration, testing, and refinement.<br><br>Just be wary of perfectionism. Push yourself to share your creations with others. By maintaining an open stance, you’ll be able to learn from their feedback. Consider their responses new material that you can draw from the next time you’re embarking on a creative endeavor.", },
+    { menuItem: "Love The Work", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.<br><br><Image id='imgTwo' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' fluid /><div id='zoomedDivTwo' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'><div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomTwo' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(159.844px, -236px, 0px) scale(1.04653); transform-origin: center center 0px; will-change: transform, top, left; top: 263px; left: 248.156px; width: 380px; height: 380px;'>", },
     { menuItem: "Have Fun", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", },
     { menuItem: " ", pane: " ", },
     // { menuItem: " ", pane: " ??? ", },
   ]
 
-  // const jsonArray = [
-  //   { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><br><Image id='imgOne' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' fluid /><div class='zoomedDiv' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'> <div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(40.3438px, -329px, 0px) scale(1.09492); transform-origin: center center 0px; will-change: transform, top, left; top: 361px; left: 252.156px; width: 619px; height: 380px;'></div>", },
-  //   { menuItem: "Gaining Insight", pane: "To spark creativity, feed your brain material like you're cramming for a tough test. Then stop thinking about the problem you want to solve. Go surfing or take a leisurely walk. Research shows that letting your mind wander fosters creativity. <br></br>It’s also found that meditation helps you spot and solve problems in creative ways. It promotes divergent thinking that gets novel ideas flowing. According to these studies, meditation also makes you more open to considering new solutions. Time to breathe.", },
-  //   { menuItem: "Making It Real", pane: "No creative process is truly complete until it manifests a tangible reality. Whether your idea is an action or a physical creation, bringing it to life will likely involve the hard work of iteration, testing, and refinement. <br><br>Just be wary of perfectionism. Push yourself to share your creations with others. By maintaining an open stance, you’ll be able to learn from their feedback. Consider their responses new material that you can draw from the next time you’re embarking on a creative endeavor.", },
-  //   { menuItem: "Love The Work", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work. <br><br><Image id='imgTwo' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' fluid /><div class='zoomedDiv' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'><div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' style='cursor: zoom-out; position: absolute; transition: transform 300ms; transform: translate3d(159.844px, -236px, 0px) scale(1.04653); transform-origin: center center 0px; will-change: transform, top, left; top: 263px; left: 248.156px; width: 380px; height: 380px;'></div>", },
-  //
-  //   { menuItem: "Have Fun", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", },
-  //   { menuItem: " ", pane: " ", },
-  //   { menuItem: " ", pane: " ??? ", },
-  // ]
 
   const parsedPanes = jsonArray.map( (object, index) => {
     // console.log('TabBlock - object is: ', index, object)
@@ -40,18 +32,6 @@ const TabBlock = (props) => {
     newObject.id = index //giving each object an id...
     newObject.key = index //giving each object a key...
     ////////trying to add unique key to each child in array so i don't get warning in console...
-    // if (newObject.pane instanceof Array) {
-    //   console.log('adding keys to array of parsed object...')
-    // //   let cascadingMenu = (
-    // // <Menu>{items.map((item, key) => <MenuItem key={key}>{item}</MenuItem>)}</Menu> )
-    //   newObject.pane = (
-    //     newObject.pane.map( (object, index) => {
-    //     console.log(`------>>> returning ${object} with div key ${index}`)
-    //     // object.key = index
-    //     return <div key={index}>{object}</div>
-    //     })
-    //   )
-    // }
     // console.log('TabBlock - newObject is: ', newObject)
     return newObject
   })
@@ -111,11 +91,10 @@ const TabBlock = (props) => {
 
 
   function hideChevrons() {
-    console.log('no longer hovering over blocks-tabs__wrapper div, so hideChevrons called...')
-    //hideBothChevrons() //can i just call this???
+    // console.log('no longer hovering over blocks-tabs__wrapper div, so hideChevrons called...')
     var items = getMenuItems()
     for (var i = 0; i < items.length; i++) {
-      items[i].addEventListener("mouseleave", function() { //mouseover or mouseleave????
+      items[i].addEventListener("mouseleave", function() {
         hideBothChevrons()
       })
     }
@@ -140,7 +119,7 @@ const TabBlock = (props) => {
     var leftSide = windowWidth * .135
     console.log('22. leftSide is: ', leftSide)
     // if (firstItemPosition.x > 204) { //do this by 18% instead of pixels??? pixels will change with resizing window...
-    if (firstItemPosition.x > leftSide) { //do this by 18% instead of pixels??? pixels will change with resizing window...
+    if (firstItemPosition.x > leftSide) {
       hideChevronLeft()
     } else {
       chevrons[0].classList.remove("hidden")
@@ -190,42 +169,36 @@ const TabBlock = (props) => {
 
       <h2 className="brand--head">Tab Block</h2>
 
-      {/* <Modal trigger={<Button>Show Modal</Button>}>
-      <Modal.Header>Select a Photo</Modal.Header>
-      <Modal.Content image>
-        <Image src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' />
-        modal image here
-      </Modal.Content>
-    </Modal> */}
+      <div className="blocks-tabs">
+        <div className="blocks-tabs__wrapper">
+          <FontAwesomeIcon
+            icon={Icons.faChevronLeft}
+            onClick={() => clickedLeftChevron()}
+            className="chevron-icon hidden"
+            size="4x"
+          />
 
-    <div className="blocks-tabs__wrapper">
-      <FontAwesomeIcon
-        icon={Icons.faChevronLeft}
-        onClick={() => clickedLeftChevron()}
-        className="chevron-icon hidden"
-        size="4x"
-      />
+          <FontAwesomeIcon
+            icon={Icons.faChevronRight}
+            onClick={() => clickedRightChevron()}
+            className="chevron-icon hidden chevron-icon__right"
+            size="4x"
+          />
 
-      <FontAwesomeIcon
-        icon={Icons.faChevronRight}
-        onClick={() => clickedRightChevron()}
-        className="chevron-icon hidden chevron-icon__right"
-        size="4x"
-      />
+          <Tab
+            menu={{attached: true, tabular: false}}
+            panes={parsedPanes}
+            renderActiveOnly={false}
+            onMouseEnter={() => showChevrons()}
+            onMouseLeave={() => hideChevrons()}
+            onClick={props.toggleImageState}
+            // onClick={function(e){console.log(e.target, e.currentTarget);}}
+          />
+        </div>
+      </div>
 
-      <Tab
-        menu={{attached: true, tabular: false}}
-        panes={parsedPanes}
-        renderActiveOnly={false}
-        onMouseEnter={() => showChevrons()}
-        onMouseLeave={() => hideChevrons()}
-        onClick={props.toggleImageState}
-        // onClick={function(e){console.log(e.target, e.currentTarget);}}
-      />
     </div>
-
-  </div>
-)
+  )
 }
 
 export default TabBlock
