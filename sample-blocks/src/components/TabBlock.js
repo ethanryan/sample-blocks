0735a26a8fb3522971ae3375f5a632e3af9bdf0c
ethanryan <ethanryan@gmail.com>
2018-03-01 12:30:40 -0500

    moved jsonArray to BlocksContainer

--- sample-blocks/src/components/TabBlock.js
+++ sample-blocks/src/components/TabBlock.js
@@ -9,20 +9,10 @@ require('typeface-merriweather')
 
 
 const TabBlock = (props) => {
-  // console.log('TabBlock props: ', props)
+  console.log('--------TabBlock props: ', props)
 
-  const jsonArray = [
-    { menuItem: "Embracing Discovery", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.<br><br><Image id='imgOne' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif' fluid /><div id='zoomedDivOne' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'> <div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomOne' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/q0r7xIVMCo4RkD5A.gif'></div>", },
-    { menuItem: "Gaining Insight", pane: "To spark creativity, feed your brain material like you're cramming for a tough test. Then stop thinking about the problem you want to solve. Go surfing or take a leisurely walk. Research shows that letting your mind wander fosters creativity. <br></br>It’s also found that meditation helps you spot and solve problems in creative ways. It promotes divergent thinking that gets novel ideas flowing. According to these studies, meditation also makes you more open to considering new solutions. Time to breathe.", },
-    { menuItem: "Making It Real", pane: "No creative process is truly complete until it manifests a tangible reality. Whether your idea is an action or a physical creation, bringing it to life will likely involve the hard work of iteration, testing, and refinement.<br><br>Just be wary of perfectionism. Push yourself to share your creations with others. By maintaining an open stance, you’ll be able to learn from their feedback. Consider their responses new material that you can draw from the next time you’re embarking on a creative endeavor.", },
-    { menuItem: "Love The Work", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.<br><br><Image id='imgTwo' className='figure-image__image' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png' fluid /><div id='zoomedDivTwo' class='display-none' style='position: fixed; top: 0px; right: 0px; bottom: 0px; left: 0px; z-index: 999;'><div style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; background-color: rgb(255, 255, 255); opacity: 1; transition: opacity 300ms;'></div><img id='imgZoomTwo' class='figure-image__image--zoom' src='https://cdn.articulate.com/rise/courses/_Af0P0L1E-1akg7PhqRPNyg0uRFD0pUp/kcA21C-HvSKNkEmO.png'>", },
-    { menuItem: "Have Fun", pane: "Every creative endeavor requires that you take risks. If you try and don't succeed, you've still learned something. It took Thomas Edison more than 10,000 tries to invent a viable lightbulb. You're not failing. You're discovering what doesn't work.", },
-    { menuItem: " ", pane: " ", },
-  ]
-
-
-  const parsedPanes = jsonArray.map( (object, index) => {
-    // console.log('TabBlock - object is: ', index, object)
+  // const parsedPanes = jsonArray.map( (object, index) => { //moved jsonArray to container...
+  const parsedPanes = props.jsonArray().map( (object, index) => {
     var newObject = Object.assign({}, object) //cloning each object into a newObject
     // newObject.key = index
     newObject.menuItem = object.menuItem
@@ -93,36 +83,6 @@ const TabBlock = (props) => {
     }
   }
 
-
-  // working on this
-  // working on this
-  // working on this
-  // working on this
-  // function styleItemGradients() {
-  //   console.log('styleItemGradients called !!!!!!!')
-  //   var anchors = document.getElementsByTagName('a')
-  //   console.log('anchors is: ', anchors)
-  //   var rightAnchor = anchors[anchors.length - 1]
-  //   console.log('rightAnchor is: ', rightAnchor)
-  //
-  //   var items = getMenuItems()
-  //   var lastItem = items[items.length - 1]
-  //   var lastItemPosition = lastItem.getBoundingClientRect()
-  //   console.log('lastItemPosition is: ', lastItemPosition)
-  //
-  //   var rightLimit = lastItemPosition.width + lastItemPosition.x
-  //   console.log('rightLimit is: ', rightLimit)
-  //   var windowWidth = window.innerWidth
-  //   var rightSide = windowWidth - (windowWidth * .13)
-  //
-  //   for (var i = 0; i < anchors.length; i++) {
-  //     if (rightLimit < rightSide) {
-  //       anchors[i].classList.remove('gradient-right')
-  //     } else {
-  //       anchors[i].classList.add('gradient-right')
-  //     }
-  //   }
-  // }
 
   function showBothChevrons() {
     // console.log('showBothChevrons called...!!!!!')
